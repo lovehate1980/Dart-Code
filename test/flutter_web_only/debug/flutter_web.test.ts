bc9d2b1c667277a1c7ef72e32016659ae42c149b
Danny Tuppeny <danny@tuppeny.com>
2019-05-21 14:47:49 +0100

    Add some code for (maybe) later

--- test/flutter_web_only/debug/flutter_web.test.ts
+++ test/flutter_web_only/debug/flutter_web.test.ts
@@ -288,6 +288,8 @@ describe("flutter for web debugger", () => {
 			return;
 		}
 
+		// const openBrowserCommand = sb.stub(envUtils, "openInBrowser").resolves();
+
 		const config = await startDebugger(flutterWebHelloWorldMainFile);
 		await Promise.all([
 			dc.configurationSequence(),
@@ -295,6 +297,7 @@ describe("flutter for web debugger", () => {
 		]);
 
 		const devTools = await vs.commands.executeCommand("dart.openDevTools") as { url: string, dispose: () => void };
+		// assert.ok(openBrowserCommand.calledOnce);
 		assert.ok(devTools);
 		assert.ok(devTools.url);
 		defer(devTools.dispose);
