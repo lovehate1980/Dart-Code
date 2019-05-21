74acd0e497e1411f4b41d1e083eb22d426313641
Danny Tuppeny <danny@tuppeny.com>
2019-05-21 12:55:21 +0100

    Revert "Fix lint"
    This reverts commit 3c7c8b7f573016103bc5f3f626461247f957a226.

--- src/decorations/flutter_ui_guides_decorations.ts
+++ src/decorations/flutter_ui_guides_decorations.ts
@@ -34,6 +34,7 @@ export class FlutterUiGuideDecorations implements vs.Disposable {
 			this.tracker.onGuidesChanged(([doc, guides]) => this.buildFromUpdatedGuides(doc, guides));
 		}
 
+
 		// Update the current visible editor when we were registered.
 		if (vs.window.activeTextEditor)
 			this.buildForTextEditor(vs.window.activeTextEditor);
