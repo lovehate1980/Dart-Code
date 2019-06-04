22c5d205619923eba8e4b895259d6e6bb20f54fb
Danny Tuppeny <danny@tuppeny.com>
2019-06-04 17:04:47 +0100

    Tidy up closing labels

--- src/extension/commands/analyzer.ts
+++ src/extension/commands/analyzer.ts
@@ -1,6 +1,7 @@
 import * as vs from "vscode";
-import { Analyzer } from "../analysis/analyzer";
 import { openInBrowser } from "../../shared/vscode/utils";
+import { Analyzer } from "../analysis/analyzer";
+import { DiagnosticServerRequest } from "../lsp/custom_protocol";
 import { lspClient } from "../lsp/setup";
 
 export class AnalyzerCommands {
@@ -10,7 +11,7 @@ export class AnalyzerCommands {
 			openInBrowser(`http://127.0.0.1:${res.port}/`);
 
 			if (lspClient) {
-				const diagServer = await lspClient.sendRequest<{ port: number }>("dart/diagnosticServer");
+				const diagServer = await lspClient.sendRequest(DiagnosticServerRequest.type, undefined);
 				openInBrowser(`http://127.0.0.1:${diagServer.port}`);
 			}
 		}));
